d62dc1f16beb4c5f08908a89e2add1ddac38bf0f
Aleksandra Bialas <aleksandra.bialas@student.pk.edu.pl>
2023-06-16 15:05:21 +0000

    styling the event tile

--- public/JS/createNewEvent.js
+++ public/JS/createNewEvent.js
@@ -20,7 +20,15 @@ function addEvent(start, end) {
     let events = ``;
     let duration = end - start;
     for (let m=1; m<=duration; m++){
-        events += `<div class="sports-event"></div>`;
+        events += `<div class="sports-event">    
+                        <div class="div-for-pic">
+                            <div class="picture-for-event"></div>
+                        </div>
+                        <div class="event-desc">
+                            <div class="event-name"></div>
+                            <div class="event-time"></div>
+                        </div>
+                   </div>`;
     }
     const event = document.querySelector(".day-grid-tile");
     event.innerHTML = events;
