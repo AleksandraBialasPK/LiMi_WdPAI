d5ceb5b69160883c8f53cf8c65921750584be96f
Aleksandra Bialas <aleksandra.bialas@student.pk.edu.pl>
2023-06-17 19:26:02 +0000

    changing User class

--- src/models/User.php
+++ src/models/User.php
@@ -2,53 +2,77 @@
 
 class User
 {
-    private $email, $password, $name, $surname;
+    private $userID;
+    private $roleID;
+    private $email;
+    private $password;
+    private $salt;
+    private $name;
+    private $avatar;
 
-    public function __construct(string $email, string $password, string $name, string $surname)
-    {
+    public function __construct(int $userID, int $roleID, string $email, string $password, string $salt, string $name, string $avatar) {
+        $this->userID = $userID;
+        $this->roleID = $roleID;
         $this->email = $email;
         $this->password = $password;
+        $this->salt = $salt;
         $this->name = $name;
-        $this->surname = $surname;
+        $this->avatar = $avatar;
     }
 
-    public function getEmail(): string
-    {
+    public function getUserID(): int {
+        return $this->userID;
+    }
+
+    public function setUserID(int $userID) {
+        $this->userID = $userID;
+    }
+
+    public function getRoleID(): int {
+        return $this->roleID;
+    }
+
+    public function setRoleID($roleID) {
+        $this->roleID = $roleID;
+    }
+
+    public function getEmail(): string {
         return $this->email;
     }
 
-    public function setEmail(string $email)
-    {
+    public function setEmail(string $email) {
         $this->email = $email;
     }
 
-    public function getPassword(): string
-    {
+    public function getPassword(): string {
         return $this->password;
     }
 
-    public function setPassword(string $password)
-    {
+    public function setPassword(string $password) {
         $this->password = $password;
     }
 
-    public function getName(): string
-    {
+    public function getSalt() {
+        return $this->salt;
+    }
+
+    public function setSalt() {
+        $this->salt = "foo";
+    }
+
+    public function getName(): string {
         return $this->name;
     }
 
-    public function setName(string $name)
-    {
+    public function setName(string $name) {
         $this->name = $name;
     }
 
-    public function getSurname(): string
-    {
-        return $this->surname;
+    public function getAvatar() {
+        return $this->avatar;
     }
 
-    public function setSurname(string $surname)
-    {
-        $this->surname = $surname;
+    public function setAvatar($avatar) {
+        $this->avatar = $avatar;
     }
 }
